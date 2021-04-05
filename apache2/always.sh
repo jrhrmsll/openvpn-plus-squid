7212ab21708deb14be12763e1afcec30d7889470
jrhrmsll <jrhermosilla.dev@gmail.com>
2021-04-05 21:47:39 +0200

    replace 192.168.10.0/24 network by 172.31.0.0/24 one

--- apache2/always.sh
+++ apache2/always.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # iptables and routes
-route add -net 192.168.10.0/24 gw 172.16.0.100 enp0s8
+route add -net 172.31.0.0/24 gw 172.16.0.100 enp0s8
--- openvpn/always.sh
+++ openvpn/always.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # iptables and routes
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o enp0s8 -j MASQUERADE
-route add -net 172.16.0.0/24 gw 192.168.10.100 enp0s8
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o enp0s9 -j MASQUERADE
+route add -net 172.16.0.0/24 gw 172.31.0.100 enp0s9
 
 if ufw status | grep "inactive"; then
   ufw default deny incoming
